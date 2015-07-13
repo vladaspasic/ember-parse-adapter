1d8b7cbd5f0df5c14627d6693725868720a752a3
Vladimir Spasic <vspasic@admins-MacBook-Pro-6.local>
2015-07-13 21:24:08 +0200

    added fixes

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -5,12 +5,12 @@ export default DS.RESTAdapter.extend({
 
   defaultSerializer: '-parse',
 
-  init: function(){
+  init: function() {
     this._super();
 
-    this.set( 'headers', {
-      'X-Parse-Application-Id' : Ember.get( this, 'applicationId' ),
-      'X-Parse-REST-API-Key'   : Ember.get( this, 'restApiId' )
+    this.set('headers', {
+      'X-Parse-Application-Id': Ember.get(this, 'applicationId'),
+      'X-Parse-REST-API-Key': Ember.get(this, 'restApiId')
     });
   },
 
@@ -20,164 +20,213 @@ export default DS.RESTAdapter.extend({
 
   classesPath: 'classes',
 
-  pathForType: function( type ) {
-    if ( 'parseUser' === type ) {
+  pathForType: function(type) {
+    if ('parseUser' === type) {
       return 'users';
-    } else if ( 'login' === type ) {
+    } else if ('login' === type) {
       return 'login';
     } else {
-      return this.classesPath + '/' + this.parsePathForType( type );
+      return this.classesPath + '/' + this.parsePathForType(type);
     }
   },
 
   // Using TitleStyle is recommended by Parse
   // @TODO: test
-  parsePathForType: function( type ) {
-    return Ember.String.capitalize( Ember.String.camelize( type ) );
+  parsePathForType: function(type) {
+    return Ember.String.capitalize(Ember.String.camelize(type));
   },
 
   /**
-  * Because Parse doesn't return a full set of properties on the
-  * responses to updates, we want to perform a merge of the response
-  * properties onto existing data so that the record maintains
-  * latest data.
-  */
-  createRecord: function( store, type, record ) {
-    var serializer = store.serializerFor( type.typeKey ),
-      snapshot   = record._createSnapshot(),
-      data       = {},
-      adapter    = this;
+   * Because Parse doesn't return a full set of properties on the
+   * responses to updates, we want to perform a merge of the response
+   * properties onto existing data so that the record maintains
+   * latest data.
+   */
+  createRecord: function(store, type, record) {
+    var serializer = store.serializerFor(type.typeKey),
+      snapshot = record._createSnapshot(),
+      data = {};
 
-    serializer.serializeIntoHash( data, type, snapshot, { includeId: true } );
-
-    return new Ember.RSVP.Promise( function( resolve, reject ) {
-      adapter.ajax( adapter.buildURL( type.typeKey ), 'POST', { data: data } ).then(
-        function( json ) {
-          var completed = Ember.merge( data, json );
-          resolve( completed );
-        },
-        function( reason ) {
-          reject( reason.responseJSON );
-          }
-      );
+    serializer.serializeIntoHash(data, type, snapshot, {
+      includeId: true
     });
+
+    return this.ajax(this.buildURL(type.typeKey), 'POST', {
+        data: data
+      }).then(function(json) {
+        return Ember.merge(data, json);
+      });
   },
 
   /**
-  * Because Parse doesn't return a full set of properties on the
-  * responses to updates, we want to perform a merge of the response
-  * properties onto existing data so that the record maintains
-  * latest data.
-  */
+   * Because Parse doesn't return a full set of properties on the
+   * responses to updates, we want to perform a merge of the response
+   * properties onto existing data so that the record maintains
+   * latest data.
+   */
   updateRecord: function(store, type, record) {
-    var serializer  = store.serializerFor( type.typeKey ),
-      snapshot    = record._createSnapshot(),
-      id          = record.get( 'id' ),
+    var serializer = store.serializerFor(type.typeKey),
+      snapshot = record._createSnapshot(),
+      id = record.get('id'),
       sendDeletes = false,
-      deleteds    = {},
-      data        = {},
-      adapter     = this;
+      deleteds = {},
+      data = {},
+      adapter = this;
 
-    serializer.serializeIntoHash(data, type, snapshot, { includeId: true });
+    serializer.serializeIntoHash(data, type, snapshot, {
+      includeId: true
+    });
 
-    type.eachRelationship(function( key ) {
-      if ( data[key] && data[key].deleteds ) {
+    type.eachRelationship(function(key) {
+      if (data[key] && data[key].deleteds) {
         deleteds[key] = data[key].deleteds;
         delete data[key].deleteds;
         sendDeletes = true;
       }
     });
 
-    return new Ember.RSVP.Promise( function( resolve, reject ) {
-      if ( sendDeletes ) {
-        adapter.ajax( adapter.buildURL( type.typeKey, id ), 'PUT', { data: deleteds } ).then(
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (sendDeletes) {
+        adapter.ajax(adapter.buildURL(type.typeKey, id), 'PUT', {
+          data: deleteds
+        }).then(
           function() {
-            adapter.ajax( adapter.buildURL( type.typeKey, id ), 'PUT', { data: data } ).then(
-              function( updates ) {
+            adapter.ajax(adapter.buildURL(type.typeKey, id), 'PUT', {
+              data: data
+            }).then(
+              function(updates) {
                 // This is the essential bit - merge response data onto existing data.
-                resolve( Ember.merge( data, updates ) );
+                resolve(Ember.merge(data, updates));
               },
-              function( reason ) {
-                reject( 'Failed to save parent in relation: ' + reason.response.JSON );
+              function(reason) {
+                reject('Failed to save parent in relation: ' + reason.response.JSON);
               }
             );
-          },
-          function( reason ) {
-            reject( reason.responseJSON );
-          }
-        );
+          }, reject);
 
       } else {
-        adapter.ajax( adapter.buildURL( type.typeKey, id ), 'PUT', { data: data } ).then(
-          function( json ) {
+        adapter.ajax(adapter.buildURL(type.typeKey, id), 'PUT', {
+          data: data
+        }).then(
+          function(json) {
             // This is the essential bit - merge response data onto existing data.
-            resolve( Ember.merge( data, json ) );
-          },
-          function( reason ) {
-            reject( reason.responseJSON );
-          }
-        );
+            resolve(Ember.merge(data, json));
+          }, reject);
       }
     });
   },
 
-  parseClassName: function (key ) {
-    return Ember.String.capitalize( key );
+  parseClassName: function(key) {
+    return Ember.String.capitalize(key);
+  },
+
+  find: function(store, type, id, snapshot) {
+    var query = {};
+
+    this.inlcudeRelationships(store, type, query);
+
+    return this.ajax(this.buildURL(type.typeKey, id, snapshot), 'GET', {
+      data: query
+    });
+  },
+
+  findAll: function(store, type, sinceToken) {
+    var query = {};
+
+    this.inlcudeRelationships(store, type, query);
+
+    if (sinceToken) {
+      query.since = sinceToken;
+    }
+
+    return this.ajax(this.buildURL(type.typeKey), 'GET', {
+      data: query
+    });
   },
 
   /**
-  * Implementation of a hasMany that provides a Relation query for Parse
-  * objects.
-  */
-  findHasMany: function( store, record, relatedInfo ) {
-    var relatedInfo_ = JSON.parse( relatedInfo ),
-        query        = {
+   * Implementation of findQuery that automatically wraps query in a
+   * JSON string.
+   *
+   * @example
+   *     this.store.find('comment', {
+   *       where: {
+   *         post: {
+   *             "__type":  "Pointer",
+   *             "className": "Post",
+   *             "objectId": post.get('id')
+   *         }
+   *       }
+   *     });
+   */
+  findQuery: function(store, type, query) {
+    if (this.sortQueryParams) {
+      query = this.sortQueryParams(query);
+    }
+
+    this.inlcudeRelationships(store, type, query);
+
+    if (query.where && 'string' !== Ember.typeOf(query.where)) {
+      query.where = JSON.stringify(query.where);
+    }
+
+    return this.ajax(this.buildURL(type.typeKey), 'GET', {
+      data: query
+    });
+  },
+
+  /**
+   * Implementation of a hasMany that provides a Relation query for Parse
+   * objects.
+   */
+  findHasMany: function(store, record, relatedInfo) {
+    var relatedInfo_ = JSON.parse(relatedInfo),
+      query = {
         where: {
           '$relatedTo': {
             'object': {
-              '__type'    : 'Pointer',
-              'className' : this.parseClassName( record.typeKey ),
-              'objectId'  : record.get( 'id' )
+              '__type': 'Pointer',
+              'className': this.parseClassName(record.typeKey),
+              'objectId': record.get('id')
             },
             key: relatedInfo_.key
           }
         }
-    };
+      };
 
     // the request is to the related type and not the type for the record.
     // the query is where there is a pointer to this record.
-    return this.ajax( this.buildURL( relatedInfo_.typeKey ), "GET", { data: query } );
+    return this.ajax(this.buildURL(relatedInfo_.typeKey), "GET", {
+      data: query
+    });
   },
 
   /**
-  * Implementation of findQuery that automatically wraps query in a
-  * JSON string.
-  *
-  * @example
-  *     this.store.find('comment', {
-  *       where: {
-  *         post: {
-  *             "__type":  "Pointer",
-  *             "className": "Post",
-  *             "objectId": post.get('id')
-  *         }
-  *       }
-  *     });
-  */
-  findQuery: function ( store, type, query ) {
-    if ( query.where && 'string' !== Ember.typeOf( query.where ) ) {
-      query.where = JSON.stringify( query.where );
-    }
+   * Inlcudes the Pointers that are not set as `async` in the
+   * query.
+   *
+   */
+  inlcudeRelationships: function(store, type, query) {
+    var includes = [];
 
-    // Pass to _super()
-    return this._super( store, type, query );
+    type.eachRelationship(function(name, descriptor) {
+      var options = descriptor.options;
+
+      if (options.async !== true && options.relationship !== true) {
+        includes.push(name);
+      }
+    }, this);
+
+    if (!Ember.isEmpty(includes)) {
+      query.include = includes.join(',');
+    }
   },
 
-  sessionToken: Ember.computed( 'headers.X-Parse-Session-Token', function( key, value ) {
-    if ( arguments.length < 2 ) {
-      return this.get( 'headers.X-Parse-Session-Token' );
+  sessionToken: Ember.computed('headers.X-Parse-Session-Token', function(key, value) {
+    if (arguments.length < 2) {
+      return this.get('headers.X-Parse-Session-Token');
     } else {
-      this.set( 'headers.X-Parse-Session-Token', value );
+      this.set('headers.X-Parse-Session-Token', value);
       return value;
     }
   })
--- addon/serializers/application.js
+++ addon/serializers/application.js
@@ -1,6 +1,34 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+// Constructs the Related Record
+function buildRelatedRecord(store, serializer, type, item) {
+  if ('Pointer' === item.__type) {
+    return item.objectId;
+  } else {
+    item.id = item.objectId;
+
+    delete item.__type;
+    delete item.className;
+    delete item.objectId;
+
+    item.type = type;
+    serializer.normalizeAttributes(type, item);
+    serializer.normalizeRelationships(type, item);
+    return store.push(type, item);
+  }
+}
+
+// Extract the related recors as an Array of ID's
+function extractIdsForHasMany(hash, key) {
+  var payload = hash[key] || {};
+  payload.objects = payload.objects || [];
+
+  hash[key] = payload.objects.map(function(obj) {
+    return obj.objectId;
+  });
+}
+
 export default DS.RESTSerializer.extend({
 
   primaryKey: 'objectId',
@@ -67,67 +95,45 @@ export default DS.RESTSerializer.extend({
   * side of the "hasMany".
   */
   normalizeRelationships: function( type, hash ) {
-    var store      = this.get('store'),
-      serializer = this;
-
-    type.eachRelationship( function( key, relationship ) {
+    type.eachRelationship(function(key, relationship) {
+      var store = this.get('store'),
+        serializer = this;
 
       var options = relationship.options;
 
-      // Handle the belongsTo relationships
-      if ( hash[key] && 'belongsTo' === relationship.kind ) {
-        hash[key] = hash[key].objectId;
+      if (hash[key] && 'belongsTo' === relationship.kind) {
+        hash[key] = buildRelatedRecord(store, serializer, relationship.type, hash[key]);
       }
 
-      // Handle the hasMany relationships
-      if ( hash[key] && 'hasMany' === relationship.kind ) {
-
-        // If this is a Relation hasMany then we need to supply
-        // the links property so the adapter can async call the
-        // relationship.
-        // The adapter findHasMany has been overridden to make use of this.
-        if(options.relation) {
+      // If this is a Relation hasMany then we need to supply
+      // the links property so the adapter can async call the
+      // relationship.
+      // The adapter findHasMany has been overridden to make use of this.
+      if (hash[key] && 'hasMany' === relationship.kind) {
+        if (options.relation) {
           // hash[key] contains the response of Parse.com: eg {__type: Relation, className: MyParseClassName}
-          // this is an object that make ember-data fail, as it expects nothing or an array ids that represent the records
-          hash[key] = [];
+          extractIdsForHasMany(hash, key);
 
-          // ember-data expects the link to be a string
-          // The adapter findHasMany will parse it
           if (!hash.links) {
             hash.links = {};
           }
 
-          hash.links[key] = JSON.stringify({typeKey: relationship.type.typeKey, key: key});
+          hash.links[key] = JSON.stringify({
+            typeKey: relationship.type.typeKey,
+            key: key
+          });
         }
 
-        if ( options.array ) {
-          // Parse will return [null] for empty relationships
-          if ( hash[key].length && hash[key] ) {
-            hash[key].forEach( function( item, index, items ) {
-              // When items are pointers we just need the id
-              // This occurs when request was made without the include query param.
-              if ( 'Pointer' === item.__type ) {
-                items[index] = item.objectId;
-
-              } else {
-                // When items are objects we need to clean them and add them to the store.
-                // This occurs when request was made with the include query param.
-                delete item.__type;
-                delete item.className;
-                item.id = item.objectId;
-                delete item.objectId;
-                item.type = relationship.type;
-                serializer.normalizeAttributes( relationship.type, item );
-                serializer.normalizeRelationships( relationship.type, item );
-                store.push( relationship.type, item );
-              }
+        if (options.array) {
+          if (hash[key].length && hash[key]) {
+            hash[key].forEach(function(item, index, items) {
+              items[index] = buildRelatedRecord(store, serializer, relationship.type, item);
             });
           }
         }
       }
-    }, this );
 
-    this._super( type, hash );
+    }, this);
   },
 
   serializeIntoHash: function( hash, type, snapshot, options ) {
@@ -170,55 +176,66 @@ export default DS.RESTSerializer.extend({
     }
   },
 
-  serializeHasMany: function( snapshot, json, relationship ) {
-    var key   = relationship.key,
-      hasMany = snapshot.hasMany( key ),
+  serializeHasMany: function(snapshot, json, relationship) {
+    var key = relationship.key,
+      hasMany = snapshot.hasMany(key),
       options = relationship.options,
-      _this   = this;
+      _this = this;
 
-    if ( hasMany && hasMany.get( 'length' ) > 0 ) {
-      json[key] = { 'objects': [] };
+    if (hasMany) {
+      json[key] = {
+        'objects': []
+      };
 
-      if ( options.relation ) {
+      if (options.relation) {
         json[key].__op = 'AddRelation';
       }
 
-      if ( options.array ) {
+      if (options.array) {
         json[key].__op = 'AddUnique';
       }
 
-      hasMany.forEach( function( child ) {
+      hasMany.forEach(function(child) {
         json[key].objects.push({
-          '__type'    : 'Pointer',
-          'className' : _this.parseClassName(child.type.typeKey),
-          'objectId'  : child.attr( 'id' )
+          '__type': 'Pointer',
+          'className': _this.parseClassName(child.type.typeKey),
+          'objectId': child.id
         });
       });
 
-      if ( hasMany._deletedItems && hasMany._deletedItems.length ) {
-        if ( options.relation ) {
-          var addOperation    = json[key],
-            deleteOperation = { '__op': 'RemoveRelation', 'objects': [] };
+      if (hasMany._deletedItems && hasMany._deletedItems.length) {
+        if (options.relation) {
+          var addOperation = json[key],
+            deleteOperation = {
+              '__op': 'RemoveRelation',
+              'objects': []
+            };
 
-          hasMany._deletedItems.forEach( function( item ) {
+          hasMany._deletedItems.forEach(function(item) {
             deleteOperation.objects.push({
-              '__type'    : 'Pointer',
-              'className' : item.type,
-              'objectId'  : item.id
+              '__type': 'Pointer',
+              'className': item.type,
+              'objectId': item.id
             });
           });
 
-          json[key] = { '__op': 'Batch', 'ops': [addOperation, deleteOperation] };
+          json[key] = {
+            '__op': 'Batch',
+            'ops': [addOperation, deleteOperation]
+          };
         }
 
-        if ( options.array ) {
-          json[key].deleteds = { '__op': 'Remove', 'objects': [] };
+        if (options.array) {
+          json[key].deleteds = {
+            '__op': 'Remove',
+            'objects': []
+          };
 
-          hasMany._deletedItems.forEach( function( item ) {
+          hasMany._deletedItems.forEach(function(item) {
             json[key].deleteds.objects.push({
-              '__type'    : 'Pointer',
-              'className' : item.type,
-              'objectId'  : item.id
+              '__type': 'Pointer',
+              'className': item.type,
+              'objectId': item.id
             });
           });
         }
