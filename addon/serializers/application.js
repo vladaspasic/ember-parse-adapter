d6051db87ce1b33cced9a3397f96e7c3cc2c4a56
Vladimir Spasic <vspasic@CI002060.local>
2016-01-15 16:49:23 +0100

    add primary key name

--- addon/serializers/application.js
+++ addon/serializers/application.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.reopen({
-
+  primaryKey: 'objectId',
+  
   /**
    * Generate a type name from Parse Class name
    *
